9ff47ffcd44ccaac77f4b59713c37e92aab441cd
Abdallah <ahkortam@gmail.com>
2022-03-06 18:20:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { nanoid } from 'nanoid';
 
 function App() {
+
+  const [input, setInput] = React.useState("")
+  const [todos, setTodos] = React.useState([])
+
+
+
+  React.useEffect(() => {
+    const temp = localStorage.getItem("savedTodos")
+    const loadedTodos = JSON.parse(temp)
+
+    if (loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("savedTodos", temp)
+
+  }, [todos])
+
+
+
+  function handleChange(e) {
+    setInput(e.target.value)
+  }
+
+  function hnadleSubmit(e) {
+    e.preventDefault()
+
+    const newTodo = {
+      id: nanoid(),
+      text: input
+    }
+    if (newTodo.text.trim()) {
+
+      setTodos([...todos, newTodo])
+    }
+
+    setInput("")
+  }
+
+
+  function deleteTodo(id) {
+    const updateTodos = todos.filter(todo => todo.id !== id)
+    setTodos(updateTodos)
+  }
+
+
+
+  const elements = todos.map(todo =>
+
+    <div key={todo.id} className="todos">
+      <p>{todo.text}</p>
+      <div className='btns'>
+        <i onClick={() => deleteTodo(todo.id)} className="fa-solid fa-trash"></i>
+      </div>
+    </div>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={hnadleSubmit}>
+        <p id='title'>TODO</p>
+        <div className='inputs'>
+          <input onChange={handleChange} value={input} placeholder='type...'></input>
+          <button id='delete'>
+            <i id="del" className="fa-solid fa-plus"></i>
+          </button>
+        </div>
+      </form>
+
+      {elements}
+
     </div>
   );
 }
